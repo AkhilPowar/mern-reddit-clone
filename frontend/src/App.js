5a50b801b4b9a1f714082138c9305c2df0544550
Akhil Powar <akhilpowar16@gmail.com>
2020-06-21 23:05:40 +0530

    Fix eslint version
    - Fixed error of react-script start not running due to incorrect eslint version
    - Add .vscode folder to git ignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
+import TextContent from "./components/card/TextContent";
 import "./App.css";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <TextContent 
+            title="New Post"
+            time="2020/02/02"
+            voteCount="1.3k"
+            content="Hello World"
+        />
     );
 }
 
